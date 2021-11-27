690cbe8201cb2b98d0da150e457eb25dd31c2b37
Pedro <pedroxavier2001@gmail.com>
2021-11-27 14:55:48 -0300

    fix animation

--- static/javascript/navbar.js
+++ static/javascript/navbar.js
@@ -1,22 +1,65 @@
 var inputBox = document.getElementById("inputBox");
 var hideableElements = document.getElementsByClassName("hideable");
+var expandeableElement = document.getElementById("expandeable");
+var contractedWidth = expandeableElement.offsetWidth;
+var speed = 1.25;
 
 // A CORRIGIR: 
 // Se clicar rápido fora e dentro da barra de pesquisa, não funciona mais como o esperado
+
+function expandBar(element){
+    var currentWidth = element.offsetWidth;
+
+    function increase()
+    {
+        currentWidth += speed * 15;
+
+        if (currentWidth >= 600)
+        {
+            element.style.maxWidth = 600 + "px";
+            return true
+        }
+
+        element.style.maxWidth = currentWidth + "px";
+        requestAnimationFrame(increase);
+    }
+
+    increase();
+}
+
+function contractBar(element){
+    var currentWidth = element.offsetWidth;
+
+    function decrease()
+    {
+        currentWidth -= speed * 15;
+
+        if (currentWidth <= contractedWidth)
+        {
+            element.style.maxWidth = contractedWidth + "px";
+            return fadeIn(hideableElements, hideableElements.length-1)
+        }
+
+        element.style.maxWidth = currentWidth + "px";
+        requestAnimationFrame(decrease);
+    }
+
+    decrease();
+}
 
 function fadeOut(elements, i){
     var op = 1; // largura inicial
 
     function decrease()
     {   
-        op -= 0.05;
+        op -= speed * 0.1;
 
         if(op <= 0)
         {
             elements[i].style.opacity = 0;
             elements[i].style.display = "none";
             i += 1
-            if(i == elements.length){return true;}
+            if(i == elements.length){return expandBar(expandeableElement);}
             else{return fadeOut(elements, i)}
         }
         
@@ -32,13 +75,13 @@ function fadeIn(elements, i){
 
     function increase()
     {   
-        op += 0.05;
+        op += speed * 0.1;
 
         if(op >= 1)
         {
             elements[i].style.opacity = 1;
             i -= 1
-            if(i== -1){return true;}
+            if(i== -1){return true}
             else{return fadeIn(elements, i)}
         }
         
@@ -51,17 +94,26 @@ function fadeIn(elements, i){
 
 function show()
 {
-    fadeIn(hideableElements, hideableElements.length-1);
+    for(let i = 0; i < hideableElements.length; i++)
+    {
+        hideableElements[i].style.opacity = 0;
+        hideableElements[i].style.display = 'none';
+    }
+    expandeableElement.style.maxWidth = 600 + "px";
+    contractBar(expandeableElement);
 }
 
 function hide()
 {
+
     fadeOut(hideableElements, 0);
 }
 
 if (screen.width <= 600)
 {
+    expandeableElement.style.maxWidth = expandeableElement.offsetWidth  + "px";
+    
     inputBox.addEventListener("focus", hide);
-
+    
     inputBox.addEventListener("focusout", show);
 }
