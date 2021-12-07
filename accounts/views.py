28d58f10a741123fc62c633c42303252d9a2fe87
hassanvilela <hassan.vilela@hotmail.com>
2021-12-07 13:55:02 -0300

    update login

--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserCreationForm()
+
+    context = {'form': form}
+    return render(request, 'accounts/signup.html', context)
