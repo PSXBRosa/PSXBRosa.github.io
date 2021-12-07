b06f4a27b3c9bcdf414ade4731064535a883aacb
Pedro Sacramento Xavier Barreto Rosa <pedro_sxbr@usp.br>
2021-12-07 19:05:32 -0300

    add bootstrap workarounds

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from app.models import Departamento
 
 class Signupform(UserCreationForm):
+
     email = forms.EmailField(max_length=200)
     nome = forms.CharField(max_length=255)
     ano_de_ingresso = forms.IntegerField()
@@ -13,13 +14,25 @@ class Signupform(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
+
+            # esconde os textos de auxílio dentro do hover das tags
             help_text = self.fields[field].help_text
             self.fields[field].help_text = None
             if help_text != '':
+
+                # formatação do texto de auxílio
                 tags_to_remove = [("<li>",""),("</li>","\n"),("<ul>",""),("</ul>","")]
                 for tag in tags_to_remove:
                     help_text = help_text.replace(tag[0],tag[1])
+
                 self.fields[field].widget.attrs['title'] = help_text
+
+            # aplica estilo do django para fields que não são de seleção
+            if type(self.fields[field].widget)!=forms.widgets.Select:
+                self.fields[field].widget.attrs["class"] = "form-control"
+            # aplica estilo do django para fields que são de seleção
+            else:
+                self.fields[field].widget.attrs["class"] = "form-select"
 
 
     class Meta:
