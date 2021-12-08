830489d2c19152bc0bd08105c2bbf5818a49cc29
Pedro Rosa <pedro_sxbr@usp.br>
2021-12-08 16:15:13 -0300

    finish aval form

--- app/views.py
+++ app/views.py
@@ -3,8 +3,12 @@ from django.db.models import Avg
 from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
 from django.views import generic
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from django.forms import ValidationError
 from .models import Avaliacao
 from .forms import AvaliacaoForm
+from datetime import datetime
+
 
 class DisciplinaListView(generic.ListView):
     paginate_by = 10
@@ -36,6 +40,18 @@ def create_avaliacao(request,slug):
     if request.user.is_authenticated:
         if request.method == 'POST':
             avaliacao_form = AvaliacaoForm(request.POST)
+
+            # checando se a ultima avaliação foi há menos de cinco meses
+            query = Avaliacao.objects.filter(disciplina__slug=slug, autor=request.user).order_by("-data_de_criacao")
+            if query.exists():
+                date_format = "%H:%M:%S"
+                month = 2629800
+                print(query.values("data_de_criacao")[0])
+                diff = datetime.utcnow() - query[0].data_de_criacao.replace(tzinfo=None)
+                if diff.days < 5*30:
+                    raise ValidationError(_('Deve-se esperar 5 meses entre avaliações'),
+                                            code='invalid')
+            
             if avaliacao_form.is_valid():
                 disciplina = Disciplina.objects.get(slug=slug)
                 avaliacao = Avaliacao(autor=request.user, disciplina=disciplina,
@@ -49,8 +65,7 @@ def create_avaliacao(request,slug):
     else:
         return HttpResponseRedirect(
             reverse('login')
-        )
-
+        )        
     context = {'disciplina_slug':slug, 'avaliacao_form': avaliacao_form}
     return render(request, 'avaliacao.html', context)
 
