fb677414f10fe01446e53827e92f0238aa36b27f
Pedro Rosa <pedro_sxbr@usp.br>
2021-12-08 12:58:54 -0300

    improve aval form

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,9 @@
 from django.forms import ModelForm
+from django.forms.widgets import RadioSelect
 from .models import Avaliacao
 
 class AvaliacaoForm(ModelForm):
+   
     class Meta:
         model = Avaliacao
         fields = [
@@ -16,3 +18,10 @@ class AvaliacaoForm(ModelForm):
             'nota_3': 'Avaliações:',
             'nota_4': 'Dificuldade:' 
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+
+        CHOICES = [(i,str(i)) for i in range(1,11)]
+        for field in self.fields:
+            self.fields[field].widget = RadioSelect(choices=CHOICES)
